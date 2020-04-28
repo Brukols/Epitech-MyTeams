2cac864d258a024293c423068d1383c5b4c83ad8
Amaury <amaury.lecomte@epitech.eu>
2020-04-28 22:34:13 +0200

    :rocket: Adding select management for all client

--- server/src/server/launch_server.c
+++ server/src/server/launch_server.c
@@ -6,8 +6,46 @@
 */
 
 #include "server.h"
+#include "my_teams.h"
+#include "client.h"
+#include "user.h"
+#include <unistd.h>
+
+static void init_fds_client(list_t it_client, fd_set *readfs, fd_set *writefs)
+{
+    for (; it_client; it_client = it_client->next) {
+        client_t *client = ((client_t *)(it_client->value));
+        if (client->write_buf != NULL)
+            FD_SET(client->fd, writefs);
+        FD_SET(client->fd, readfs);
+    }
+}
+
+static void init_fds(server_t *server, fd_set *readfs, fd_set *writefs)
+{
+    FD_ZERO(readfs);
+    FD_ZERO(writefs);
+    FD_SET(server->fd, readfs);
+
+    init_fds_client(server->client, readfs, writefs);
+    list_t it_user = server->users;
+    for (; it_user; it_user = it_user->next) {
+        init_fds_client(((user_t *)(it_user->value))->client, readfs, writefs);
+    }
+}
 
 int launch_server(server_t *server)
 {
+    fd_set writefs;
+    fd_set readfs;
+
+    while (1) {
+        init_fds(server, &readfs, &writefs);
+        if (select(FD_SETSIZE, &readfs, &writefs, NULL, NULL) < 0)
+            return (FAILURE);
+        // if (translate_select(server, &readfs, &writefs) < 0)
+        //     return (FAILURE);
+    }
+    close(server->fd);
     return (0);
 }
