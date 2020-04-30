8358a2f0b8a8961df709e89b1f575e7a82f1c9a0
Amaury <amaury.lecomte@epitech.eu>
2020-04-30 11:07:39 +0200

    :construction: Add 'bonjour' command for benjamin in order to test

--- server/src/server/commands/command_help.c
+++ server/src/server/commands/command_help.c
@@ -7,7 +7,18 @@
 
 #include "server.h"
 
+static const char *response = "bonjour";
+
 int command_help(server_t *server, client_t *client, client_request_t *req, char *data)
 {
+    server_reply_t header = {200, strlen(response)};
+    bool ret;
+
+    ret = smart_buffer_add_data(client->write_buf, &header, sizeof(server_reply_t));
+    if (!ret)
+        return (FAILURE);
+    ret = smart_buffer_add_string(client->write_buf, response);
+    if (!ret)
+        return (FAILURE);
     return (SUCCESS);
 }
