df757317d924744ffefdf3cd2c3403eed6c30fe8
Benjamin Bourgeois <benjishift@gmail.com>
2020-05-01 13:06:47 +0200

    :construction: [CLIENT] finish 301 reply code

--- client/src/process/server/replies/reply_301.c
+++ client/src/process/server/replies/reply_301.c
@@ -5,15 +5,21 @@
 ** TODO: add description
 */
 
+#include <uuid/uuid.h>
 #include "myteams_client.h"
+#include "logging_client.h"
 
 int reply_301(client_t *info, server_reply_t *header)
 {
-    char message[header->message_size + 1];
-    bzero(&message, header->message_size + 1);
+    char username[32 + 1] = {0};
+    unsigned char uuid[16 + 1] = {0};
+    char unparse_uuid[37] = {0};
 
-    smart_buffer_get_data(info->server_out, &message, header->message_size);
-    // stdout ou stderr ??
-    fprintf(stdout, "%s\n", message);
+    if (!smart_buffer_get_data(info->server_out, &username, 32))
+        return (CLIENT_ERROR);
+    if (!smart_buffer_get_data(info->server_out, &uuid, 16))
+        return (CLIENT_ERROR);
+    uuid_unparse(uuid, unparse_uuid);
+    client_event_loggedout(unparse_uuid, username);
     return (CLIENT_SUCCESS);
 }
