8a64713a2cfe98439c03587695e47acd578fe801
Benjamin Bourgeois <benjishift@gmail.com>
2020-05-01 15:04:10 +0200

    :construction: [CLIENT] 319 finished

--- client/src/process/server/replies/reply_319.c
+++ client/src/process/server/replies/reply_319.c
@@ -14,14 +14,14 @@ int reply_319(client_t *info, server_reply_t *header)
     char username[32 + 1] = {0};
     unsigned char uuid[16 + 1] = {0};
     char unparse_uuid[36 + 1] = {0};
-    int status = 0;
+    char status = 0;
 
     if (!smart_buffer_get_data(info->server_out, &uuid, 16))
         return (CLIENT_ERROR);
     if (!smart_buffer_get_data(info->server_out, &username, 32))
         return (CLIENT_ERROR);
     uuid_unparse(uuid, unparse_uuid);
-    if (!smart_buffer_get_data(info->server_out, &status, sizeof(int)))
+    if (!smart_buffer_get_data(info->server_out, &status, 1))
         return (CLIENT_ERROR);
     client_print_user(unparse_uuid, username, status);
     return (CLIENT_SUCCESS);
