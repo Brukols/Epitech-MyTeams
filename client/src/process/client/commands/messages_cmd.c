de247fee99b71f1dc4d65dc1a3c916b66d27e507
Benjamin Bourgeois <benjishift@gmail.com>
2020-05-01 16:30:49 +0200

    :construction: [CLIENT] /send command added

--- client/src/process/client/commands/messages_cmd.c
+++ client/src/process/client/commands/messages_cmd.c
@@ -10,14 +10,4 @@
 
 int messages_cmd(client_t *info, char *cmd)
 {
-    client_request_t header = {MESSAGES, 32};
-    char username[32 + 1] = {0};
-    bool ret;
-    if (!get_arg(cmd, username, 32, 0))
-        return (CLIENT_ERROR);
-    ret = smart_buffer_add_data(info->server_in, &header, sizeof(client_request_t));
-    if (!ret) return (CLIENT_ERROR);
-    ret = smart_buffer_add_data(info->server_in, &username, 32);
-    if (!ret) return (CLIENT_ERROR);
-    return (CLIENT_SUCCESS);
 }
