1400a5feded65b8520ea631386af0d236a2b4c8f
Benjamin Bourgeois <benjishift@gmail.com>
2020-05-01 12:16:17 +0200

    :construction: [CLIENT] added /user command

--- client/src/process/client/commands/user_cmd.c
+++ client/src/process/client/commands/user_cmd.c
@@ -5,9 +5,24 @@
 ** TODO: add description
 */
 
+#include <uuid/uuid.h>
 #include "myteams_client.h"
+#include "reply_code.h"
 
 int user_cmd(client_t *info, char *cmd)
 {
+    client_request_t header = {USER, 16};
+    char uuid[36 + 1] = {0};
+    unsigned char parse_uuid[16 + 1] = {0};
+    bool ret;
 
+    if (!get_arg(cmd, uuid, 36, 0))
+        return (CLIENT_ERROR);
+    if (uuid_parse(uuid, parse_uuid) == -1)
+        return (CLIENT_ERROR);
+    ret = smart_buffer_add_data(info->server_in, &header, sizeof(client_request_t));
+    if (!ret) return (CLIENT_ERROR);
+    ret = smart_buffer_add_data(info->server_in, &parse_uuid, 16);
+    if (!ret) return (CLIENT_ERROR);
+    return (CLIENT_SUCCESS);
 }
--- client/src/process/client/get_arg.c
+++ client/src/process/client/get_arg.c
@@ -13,8 +13,8 @@ bool get_arg(const char *command, char *buf, int size, int arg_no)
     int actual = 0;
     int size_copied = 0;
 
-    for (int i = 0; command[i] && actual <= begin && size_copied < size; i++) {
-        if (actual == begin && command[i] != '"') {
+    for (int i = 0; command[i] && actual <= begin && size_copied <= size; i++) {
+        if (actual == begin && command[i] != '"' && size_copied < size) {
             buf[size_copied] = command[i];
             size_copied++;
         }
