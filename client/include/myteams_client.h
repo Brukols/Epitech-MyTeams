4000759f67b7a44987926b1dd6abccb410f17db5
Benjamin Bourgeois <benjishift@gmail.com>
2020-05-02 12:41:23 +0200

    :construction: [CLIENT] added 212 reply code

--- client/include/myteams_client.h
+++ client/include/myteams_client.h
@@ -111,6 +111,7 @@ int reply_201(client_t *info, server_reply_t *header);
 int reply_202(client_t *info, server_reply_t *header);
 int reply_210(client_t *info, server_reply_t *header);
 int reply_211(client_t *info, server_reply_t *header);
+int reply_212(client_t *info, server_reply_t *header);
 int reply_330(client_t *info, server_reply_t *header);
 int reply_331(client_t *info, server_reply_t *header);
 int reply_501(client_t *info, server_reply_t *header);
