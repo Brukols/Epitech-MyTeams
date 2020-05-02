0b3287fc9b766f8c15d62d03d851d358e7374022
Benjamin Bourgeois <benjishift@gmail.com>
2020-05-02 19:28:52 +0200

    ::ambulance: handle all reply code before select

--- client/src/process/server/handle_server_activities.c
+++ client/src/process/server/handle_server_activities.c
@@ -16,21 +16,28 @@ static bool if_good_reply(smart_buffer_t *buffer, void *data)
     return (false);
 }
 
-int handle_server_activities(client_t *info)
+static int handle_server_reply(client_t *info, server_reply_t header)
 {
-    server_reply_t reply = {0};
-    int ret = CLIENT_SUCCESS;
+    int ret = CLIENT_ERROR;
 
-    if (smart_buffer_get_data_if(info->server_out, &reply, sizeof(server_reply_t), &if_good_reply) == false)
-        return (CLIENT_SUCCESS);
     for (int i = 0; replies[i].code; i++) {
-        if (reply.reply == replies[i].code) {
-            ret = replies[i].ft(info, &reply);
+        if (header.reply == replies[i].code) {
+            ret = replies[i].ft(info, &header);
             break;
         }
     }
-    //Display defaut return code
     if (ret == CLIENT_ERROR)
-        fprintf(stdout, "Invalid response from server : %hu\n", reply.reply);
+        fprintf(stdout, "Invalid response from server : %hu\n", header.reply);
+    return (CLIENT_SUCCESS);
+}
+
+int handle_server_activities(client_t *info)
+{
+    server_reply_t header = {0};
+
+    while (smart_buffer_get_data_if(info->server_out, &header, sizeof(server_reply_t), &if_good_reply)) {
+        handle_server_reply(info, header);
+        bzero(&header, sizeof(server_reply_t));
+    }
     return (CLIENT_SUCCESS);
 }
