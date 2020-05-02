c0dd6eaaac9f0a65ab554d696f57bf1ad927f367
Jamil ETTEL <jamil.ettel@epitech.eu>
2020-05-02 21:11:05 +0200

    :zap: smart buffers now have dynamic sizes

--- lib/src/smart_buffer/smart_buffer_data.c
+++ lib/src/smart_buffer/smart_buffer_data.c
@@ -6,6 +6,16 @@
 */
 
 #include "smart_buffer.h"
+#include <stdio.h>
+
+static bool smart_buffer_realloc(smart_buffer_t *buff, size_t new_size)
+{
+    buff->buffer = realloc(buff->buffer, new_size);
+    if (!buff->buffer)
+        return (false);
+    buff->size = new_size;
+    return (true);
+}
 
 bool smart_buffer_add_string(smart_buffer_t *buffer, const char *str)
 {
@@ -14,14 +24,11 @@ bool smart_buffer_add_string(smart_buffer_t *buffer, const char *str)
 
 bool smart_buffer_add_data(smart_buffer_t *buff, const void *data, size_t size)
 {
-    if (smart_buffer_get_size(buff) + size > SMART_BUFFER_SIZE)
-        return (false);
-    for (size_t i = 0; i < size; i++) {
-        buff->buffer[buff->end] = (const char *){data}[i];
-        buff->end++;
-        if (buff->end == SMART_BUFFER_SIZE)
-            buff->end = 0;
-    }
+    if (smart_buffer_get_size(buff) + size > buff->size)
+        if (!smart_buffer_realloc(buff, smart_buffer_get_size(buff) + size))
+            return (false);
+    memcpy(buff->buffer + buff->end, data, size);
+    buff->end += size;
     return (true);
 }
 
@@ -29,33 +36,17 @@ bool smart_buffer_get_data_if(
     smart_buffer_t *buff, void *var, size_t size,
     bool (*fct)(smart_buffer_t *, void *))
 {
-    size_t pos = buff->start;
-
     if (size > smart_buffer_get_size(buff))
         return (false);
-    for (size_t i = 0; i < size; i++) {
-        (char *){var}[i] = buff->buffer[pos];
-        pos++;
-        if (pos == SMART_BUFFER_SIZE)
-            pos = 0;
-    }
-    if (!fct(buff, var))
+    memcpy(var, buff->buffer, size);
+    if (fct && !fct(buff, var))
         return (false);
-    buff->start += size;
-    if (buff->start >= SMART_BUFFER_SIZE)
-        buff->start -= SMART_BUFFER_SIZE;
+    buff->end -= size;
+    buff->buffer = memmove(buff->buffer, buff->buffer + size, buff->end);
     return (true);
 }
 
 bool smart_buffer_get_data(smart_buffer_t *buff, void *var, size_t size)
 {
-    if (size > smart_buffer_get_size(buff))
-        return (false);
-    for (size_t i = 0; i < size; i++) {
-        (char *){var}[i] = buff->buffer[buff->start];
-        buff->start++;
-        if (buff->start == SMART_BUFFER_SIZE)
-            buff->start = 0;
-    }
-    return (true);
+    return (smart_buffer_get_data_if(buff, var, size, NULL));
 }
