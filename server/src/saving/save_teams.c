c186237624c68c8f7c42626251a497b6cf39e584
Jamil ETTEL <jamil.ettel@epitech.eu>
2020-05-03 18:33:10 +0200

    :construction: saving team should be OK

--- server/src/saving/save_teams.c
+++ server/src/saving/save_teams.c
@@ -11,29 +11,29 @@
 static bool save_team_meta(void *item, int fd)
 {
     team_t *team = item;
-    char buffer[53];
+    char buffer[308];
     unsigned int size = list_get_size(team->subscribers);
 
-    buffer[0] = META_USER;
+    buffer[0] = META_TEAM;
     memcpy(buffer + 1, team->uuid, 16);
     memcpy(buffer + 17, team->name, 32);
-    memcpy(buffer + 49, &size, 4);
+    memcpy(buffer + 49, team->description, 255);
+    memcpy(buffer + 304, &size, 4);
     return (write_buffer(fd, buffer, sizeof(buffer)));
 }
 
 static bool save_team_data(void *item, int fd)
 {
-    user_t *user = item;
-    char type = DATA_USER;
+    team_t *team = item;
+    char type = DATA_TEAM;
     char buffer[16];
+    user_t *subscriber = NULL;
 
     if (write(fd, &type, 1) != 1)
         return (false);
-    for (list_t messages = user->messages; messages; messages=messages->next) {
-        /* msg = messages->value; */
-        /* memcpy(buffer, msg->user_uuid, 16); */
-        /* memcpy(buffer + 16, &msg->time, 8); */
-        /* memcpy(buffer + 24, msg->message, 512); */
+    for (list_t subs = team->subscribers; subs; subs = subs->next) {
+        subscriber = subs->value;
+        memcpy(buffer, subscriber->uuid, 16);
         if (!write_buffer(fd, buffer, sizeof(buffer)))
             return (false);
     }
@@ -42,21 +42,21 @@ static bool save_team_data(void *item, int fd)
 
 bool save_teams(list_t teams)
 {
-    /* team_t *team; */
-    /* char dir[128]; */
-    /* char uuid[37] = {0}; */
+    team_t *team;
+    char dir[128];
+    char uuid[37] = {0};
 
-    /* for (; teams; teams = teams->next) { */
-    /*     team = teams->value; */
-    /*     uuid_unparse(team->uuid, uuid); */
-    /*     strcpy(dir, ".save/teams/"); */
-    /*     strcat(dir, uuid); */
-    /*     strcat(dir, "/"); */
-    /*     if (!check_and_create_directory(dir) || */
-    /*         !save_single_item(team, dir, save_team_meta, save_team_data)) */
-    /*         printf("Could not save team %s\n", uuid); */
-    /*     else */
-    /*         save_channels(team->channels, dir); */
-    /* } */
+    for (; teams; teams = teams->next) {
+        team = teams->value;
+        uuid_unparse(team->uuid, uuid);
+        strcpy(dir, ".save/teams/");
+        strcat(dir, uuid);
+        strcat(dir, "/");
+        if (!check_and_create_directory(dir) ||
+            !save_single_item(team, dir, save_team_meta, save_team_data))
+            printf("Could not save team %s\n", uuid);
+        /* else */
+        /*     save_channels(team->channels, dir); */
+    }
     return (true);
 }
